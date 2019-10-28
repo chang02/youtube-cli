65d5085357c2886bcf2f0faa6c59b965c5d4807e
changyoung <changyoung6789@gmail.com>
2019-10-28 10:21:39 +0900

    fix lint

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,9 +1,9 @@
 import { Line } from 'vue-chartjs';
- 
+
 export default {
   extends: Line,
   props: ['data', 'options'],
-  mounted () {
+  mounted() {
     this.renderChart(this.data, this.options);
   },
 };
